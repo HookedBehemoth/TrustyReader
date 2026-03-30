3af68801e78d45f5895ae1983841763fb489b931
HookedBehemoth <luis@scheurenbrand.me>
2026-03-30 22:49:05 +0200

    desktop: skip dotfiles

--- desktop/src/std_fs.rs
+++ desktop/src/std_fs.rs
@@ -158,6 +158,9 @@ impl trusty_core::fs::Directory for StdDirectory {
                     let is_directory = metadata.is_dir();
                     let size = metadata.len() as usize;
                     let name = dir_entry.file_name().to_string_lossy().into_owned();
+                    if name.starts_with(".") {
+                        continue;
+                    }
                     result.push(StdDirEntry { name, size, is_directory });
                 }
                 Err(_) => return Err(embedded_io::ErrorKind::InvalidInput),
