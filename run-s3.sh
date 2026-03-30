c5759972808319f2ab69190e6d1e1b7966b23dfb
HookedBehemoth <luis@scheurenbrand.me>
2026-03-30 22:37:18 +0200

    fix cross compilation

--- run-s3.sh
+++ run-s3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-cargo espflash save-image --release --chip=esp32s3 --target=xtensa-esp32s3-none-elf --package trusty-s3 --bin trusty-s3 firmware.bin
+cargo espflash save-image -Z build-std=alloc,core --release --chip=esp32s3 --target=xtensa-esp32s3-none-elf --package trusty-s3 --bin trusty-s3 firmware.bin
 if [[ $(stat -c%s firmware.bin) -gt 6553600 ]]; then
     echo -e "\033[0;31m[ERROR] Firmware size exceeds OFW partition limit!"
     exit 1
